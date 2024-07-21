6071bfde204c1c0d4ac96b3e1911460a74b2806d
Taylor Blanche <john.t.blanche@gmail.com>
2024-07-21 09:48:10 -0700

    added in initial calculated value, precision value classes and also added speed and inputs checking to Grief (the main character)

---
+++ InDevelopment/Scripts/Resources/calculated_value.gd
@@ -0,0 +1,29 @@
+@tool
+extends Resource
+class_name CalculatedValue
+
+var _initial_value: PrecisionValue = PrecisionValue.new()
+var _calculated_value: PrecisionValue = PrecisionValue.new()
+var value : float:
+	get:
+		return _calculated_value.value
+var formatted_value : String:
+	get:
+		return _calculated_value.formatted_value
+
+#@export var precision: int = 0:
+	## Update speed and reset the rotation.
+	#set(new_precision):
+		#if new_precision == precision:
+			#return
+		#precision = new_precision
+		#if Engine.is_editor_hint():
+			#pass
+
+func _init(
+	initial_value: float = 0.0,
+	precision: int = 0
+):
+	_initial_value = PrecisionValue.new(initial_value, precision)
+	_calculated_value = PrecisionValue.new(initial_value, precision)
+	
